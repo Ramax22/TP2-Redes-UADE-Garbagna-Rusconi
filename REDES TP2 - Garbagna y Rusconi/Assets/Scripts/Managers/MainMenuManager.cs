e59f7481bf1edc26dea50921e4023d6fc2de3783
Ramax22 <Ramagarbagna@outlook.com>
2020-11-11 15:50:58 -0300

    avance en la pantalla de login y register

--- REDES TP2 - Garbagna y Rusconi/Assets/Scripts/Managers/MainMenuManager.cs
+++ REDES TP2 - Garbagna y Rusconi/Assets/Scripts/Managers/MainMenuManager.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
+
+/*
+    https://web.microsoftstream.com/video/5c917b19-cc6e-48b1-ad37-11c43560a635
+    time. 02:26:00
+ */
 
 public class MainMenuManager : MonoBehaviour
 { //Maximo largo de user y contraseña: 15 caracteres
-    //Link: https://web.microsoftstream.com/video/5c917b19-cc6e-48b1-ad37-11c43560a635
-    //Me quede en el 57:00
     //Las "pantallas" del menu
     [SerializeField] GameObject _mainScreen;
     [SerializeField] GameObject _loginScreen;
     [SerializeField] GameObject _registerScreen;
 
+    //Objetos de la pantalla Login
+    [SerializeField] InputField _usernameFieldLoginScreen;
+    [SerializeField] InputField _passwordFieldLoginScreen;
+
+    //Objetos de la pantalla Register
+    [SerializeField] InputField _usernameFieldRegisterScreen;
+    [SerializeField] InputField _passwordFieldRegisterScreen;
+
+    //Variables
+    string _urlCreateUser;
+    string _urlFindUser;
+
     private void Awake()
     {
         //Establezco el orden de pantallas
         ChangeToMainScreen();
+
+        //Seteo las URL
+        _urlCreateUser = "http://localhost/tp2_redes_garbagna_rusconi/createUser.php";
+        _urlFindUser = "http://localhost/tp2_redes_garbagna_rusconi/findUser.php";
     }
 
     #region ~~~ FUNCIONES DE CAMBIO DE PANELES ~~~
@@ -45,6 +65,26 @@ public class MainMenuManager : MonoBehaviour
     }
     #endregion
 
+    #region ~~~ FUNCIONES GENERICAS ~~~
+    //Funcion que verifica si hay errores de conexion en la DB
+    void ConnectionErrorHandling(UnityWebRequest request)
+    {
+        //Si no tiene internet
+        if (request.isNetworkError)
+        {
+            Debug.Log("ERROR: Can't reach server");
+            return;
+        }
+
+        //Si no encontró la página
+        if (request.isHttpError)
+        {
+            Debug.Log("ERROR: Server not found");
+            return;
+        }
+    }
+    #endregion
+
     #region ~~~ FUNCIONES DEL PANEL LOGIN ~~~
     //Funcion para crear un usuario
     IEnumerator CreateUserRequest(string username, string password, string url)
@@ -60,17 +100,52 @@ public class MainMenuManager : MonoBehaviour
         //Espero a recibir el response
         yield return request.SendWebRequest();
 
+        //Reviso que no haya errores
+        ConnectionErrorHandling(request);
 
+        //Muestro en consola lo que devuelve la consulta
+        Debug.Log(request.downloadHandler.text);
     }
 
-    void ConnectionErrorHandling(UnityWebRequest request)
+    //Funcion que crea al usuario en la db
+    public void CreateUser()
     {
-        //Si no tiene internet
-        if (request.isNetworkError)
-        {
-            //Mostrar error
-            return;
-        }
+        //TODO: REALIZAR VERIFICACIONES DESDE FRONTEND
+
+        //Ejecuto la corutina 
+        StartCoroutine(CreateUserRequest(_usernameFieldRegisterScreen.text, _passwordFieldRegisterScreen.text, _urlCreateUser));
+
+        //TODO: QUE SE LOGUE SOLO
+    }
+    #endregion
+
+    #region ~~~ FUNCIONES DEL PANEL REGISTER ~~~
+    //Funcion para mandar el request al login y trabajar con el response
+    IEnumerator LogInRequest(string username, string password, string url)
+    {
+        //Creo el formulario
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+
+        //Mando el request
+        UnityWebRequest request = UnityWebRequest.Post(url, form);
+
+        //Espero al response
+        yield return request.SendWebRequest();
+
+        //Reviso si no hay errores de conexion
+        ConnectionErrorHandling(request);
+
+        Debug.Log(request.downloadHandler.text);
+    }
+
+    //Funcion para loguearse
+    public void LogIn()
+    {
+        //TODO: FRONTEND CHECK
+
+        StartCoroutine(LogInRequest(_usernameFieldLoginScreen.text, _passwordFieldLoginScreen.text, _urlFindUser));
     }
     #endregion
 }
