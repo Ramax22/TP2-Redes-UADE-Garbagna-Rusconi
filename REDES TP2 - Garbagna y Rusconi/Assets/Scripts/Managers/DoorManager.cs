53e070c40d40ef03d8aaeacba64f4aed421cb7a7
Ekeko <ekeko22@yahoo.com.ar>
2020-12-08 02:16:16 -0300

    Hechas puertas

---
+++ REDES TP2 - Garbagna y Rusconi/Assets/Scripts/Managers/DoorManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class DoorManager : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> _doorPos;
+    private const string _doorPrefab = "Prefabs/Door";
+
+
+    private void Awake()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        foreach (var pos in _doorPos)
+        {
+            PhotonNetwork.Instantiate(_doorPrefab, pos.position, pos.rotation);
+        }
+    }
+}
---
+++ REDES TP2 - Garbagna y Rusconi/Assets/Scripts/Scene/DoorBehaviour.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Vector3 closePosition;
+    [SerializeField] Vector3 openPosition;
+    [SerializeField] bool open;
+    [SerializeField] bool close;
+    [SerializeField] bool moving = false;
+    [SerializeField] bool isOpen;
+    [SerializeField] bool isClosed;
+
+    private void Awake()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        closePosition = transform.position;
+        openPosition = transform.position;
+        openPosition.y -= 6;
+
+        isClosed = true;
+
+        InvokeRepeating("Open", Random.Range(5, 10), Random.Range(5, 10));
+    }
+
+    private void Update()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+
+        if (moving == true)
+        { 
+            if(open == true)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, openPosition, Time.deltaTime * speed);
+                if (transform.position == openPosition)
+                {
+                    moving = false;
+                    open = false;
+                    isOpen = true;
+                }
+            }
+            else if (close == true)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, closePosition, Time.deltaTime * speed);
+                if (transform.position == closePosition)
+                {
+                    moving = false;
+                    close = false;
+                    isClosed = true;
+                }
+            }
+        }
+    }
+
+    public void Open()
+    {
+        if (isClosed == true)
+        {
+            isClosed = false;
+            moving = true;
+            open = true;
+
+            Invoke("Close", Random.Range(4, 8));
+        }
+    }
+
+    public void Close()
+    {
+        if (isOpen == true)
+        {
+            isOpen = false;
+            moving = true;
+            close = true;
+        }
+    }
+}
